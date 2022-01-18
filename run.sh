503a031a32c83f69fa91780cb2c8253878a6f1af
hassantsf <hassantsf.acd@gmail.com>
2022-01-19 01:11:40 +0330

    Implement modules

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 clear
-nasm -f elf -d ELF_TYPE asm_io.asm
-gcc -m32 -c driver.c
-nasm -f elf $1.asm
-gcc -m32 driver.o $1.o asm_io.o
-./a.out
+nasm -f elf -d ELF_TYPE asm_io.asm -o ./output/asm_io.o
+gcc -m32 -c driver.c -o ./output/driver.o
+nasm -f elf $1.asm -o ./output/$1.o
+gcc -m32 ./output/driver.o ./output/$1.o  ./output/asm_io.o -o ./output/run.out
+./output/run.out
